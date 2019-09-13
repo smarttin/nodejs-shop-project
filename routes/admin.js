534614f98a252b9c82b71019d284e94ce60a6f57
smarttin <smarttin77@gmail.com>
2019-09-13 15:37:26 +0100

    prepared for deployment

---
+++ routes/admin.js
@@ -0,0 +1,95 @@
+const express = require("express");
+
+const adminController = require("../controllers/admin");
+
+const isAuth = require("../middleware/is-auth");
+
+const { body } = require("express-validator/check");
+
+const router = express.Router();
+
+
+// /admin/add-product => GET
+router.get("/add-product", isAuth, adminController.getAddProduct);
+
+// /admin/products => GET
+router.get("/products", isAuth, adminController.getProducts);
+
+// /admin/add-product => POST
+router.post(
+  "/add-product",
+  [
+    body("title")
+      .isString()
+      .isLength({ min: 3 })
+      .trim(),
+    body("price").isFloat(),
+    body("description")
+      .isLength({ min: 3, max: 400 })
+      .trim()
+  ],
+  isAuth,
+  adminController.postAddProduct
+);
+
+router.get("/edit-product/:productId", isAuth, adminController.getEditProduct);
+
+router.post(
+  "/edit-product",
+  [
+    body("title")
+      .isString()
+      .isLength({ min: 3 })
+      .trim(),
+    body("price").isFloat(),
+    body("description")
+      .isLength({ min: 3, max: 400 })
+      .trim()
+  ],
+  isAuth,
+  adminController.postEditProduct
+);
+
+// Using IMAGEURL
+// /admin/add-product => POST
+// router.post(
+//   "/add-product",
+//   [
+//     body("title")
+//       .isString()
+//       .isLength({ min: 3 })
+//       .trim(),
+//     body("imageUrl").isURL(),
+//     body("price").isFloat(),
+//     body("description")
+//       .isLength({ min: 3, max: 400 })
+//       .trim()
+//   ],
+//   isAuth,
+//   adminController.postAddProduct
+// );
+
+// router.get("/edit-product/:productId", isAuth, adminController.getEditProduct);
+
+// router.post(
+//   "/edit-product",
+//   [
+//     body("title")
+//       .isString()
+//       .isLength({ min: 3 })
+//       .trim(),
+//     body("imageUrl").isURL(),
+//     body("price").isFloat(),
+//     body("description")
+//       .isLength({ min: 3, max: 400 })
+//       .trim()
+//   ],
+//   isAuth,
+//   adminController.postEditProduct
+// );
+
+// router.post("/delete-product", isAuth, adminController.postDeleteProduct);
+
+router.delete("/product/:productId", isAuth, adminController.deleteProduct);
+
+module.exports = router;
